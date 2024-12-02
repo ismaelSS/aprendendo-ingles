d7d09c07ec417401bddeccf85e6bc83f90ba753a
ismaelss <ismael1337sn@gmail.com>
2024-12-02 11:48:05 -0300

    :technologist: add eslint configuration

---
+++ src/functions/sortWeekMonths.ts
@@ -0,0 +1,16 @@
+export type tWeekdays = {
+  english: 'sunday' |'monday' | 'tuesday' | 'wednesday ' | 'thursday' | 'friday' | 'saturday'
+  portuguese : 'domingo' | 'segunda-feira' | 'terça-feira' | 'quarta-feira' | 'quinta-feira' | 'sexta-feira' | 'sábado'
+}
+
+export default function sortWeekMonths() {
+  const weekDays = [
+    {
+      english: 'monday',
+      portuguese: 'segunda-feira'
+    }
+  ]
+  return(
+
+  )
+}
